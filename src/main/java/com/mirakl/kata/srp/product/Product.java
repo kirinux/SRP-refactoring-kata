225f3c0331bd6308a995d34b9fc04801dd79171e
Julien Goullon <julien.goullon@mirakl.com>
2022-10-20 09:53:26 +0200

    update service

--- src/main/java/com/mirakl/kata/srp/product/Product.java
+++ src/main/java/com/mirakl/kata/srp/product/Product.java
@@ -1,4 +1,32 @@
 package com.mirakl.kata.srp.product;
 
 class Product {
+
+    private String id;
+    private int price;
+    private String description;
+
+    String getId() {
+        return id;
+    }
+
+    void setId(final String id) {
+        this.id = id;
+    }
+
+    int getPrice() {
+        return price;
+    }
+
+    void setPrice(final int price) {
+        this.price = price;
+    }
+
+    String getDescription() {
+        return description;
+    }
+
+    void setDescription(final String description) {
+        this.description = description;
+    }
 }
